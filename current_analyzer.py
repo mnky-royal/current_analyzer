3aac3d6d43106930536bdbe6c232f592ceb7c9f4
rm-mnky <rm.dev@tuta.io>
2022-01-19 13:57:27 +0100

    Fixed battery voltage csv

--- current_analyzer.py
+++ current_analyzer.py
@@ -44,7 +44,7 @@ while True:
     print("PSU Voltage:{:6.3f}V    Power:{:9.6f}W    Current:{:9.6f}mA    Battery Voltage:{:6.3f}   //Hold Esc to stop".format((bus_voltage3 + shunt_voltage3),(power3),(-current3),(bus_voltage2 + shunt_voltage2)))
     print("")
     with open(filename,'a') as f:
-        f.write(str(time.strftime("%I:%M:%S"))+",{:6.3f},{:9.6f},{:9.6f}\n".format((bus_voltage3+shunt_voltage3),(-current3/1000),(power3),(bus_voltage3+shunt_voltage3)))
+        f.write(str(time.strftime("%I:%M:%S"))+",{:6.3f},{:9.6f},{:9.6f},{:63f}\n".format((bus_voltage3+shunt_voltage3),(-current3/1000),(power3),(bus_voltage2+shunt_voltage2)))
     #detect Esc
     if keyboard.is_pressed("Esc"):
         print("Uploading")
